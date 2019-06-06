af56b226d313a138ab876f99423e2974240b4384
M. Balayssac Antoine <abalayss@insa-20069>
2019-06-06 14:20:56 +0200

    test

--- assembleur.s
+++ assembleur.s
@@ -1,84 +1,122 @@
 afc r0 0
-store 6 r0
-load r0 6
-store 0 r0
-afc r0 1
-store 6 r0
-load r0 6
-store 1 r0
-load r0 0
-store 6 r0
-afc r0 0
 store 7 r0
 load r0 7
-load r1 6
+store 0 r0
+afc r0 1
+store 7 r0
+load r0 7
+store 1 r0
+load r0 0
+store 7 r0
+afc r0 0
+store 8 r0
+load r0 8
+load r1 7
 equ r0 r0 r1
 afc r1 0
 equ r0 r0 r1
-store 6 r0
-load r1 6
-jmpc 25 r1
-afc r0 4
-store 6 r0
-load r0 6
-store 5 r0
-jmp 29
-afc r0 5
-store 6 r0
-load r0 6
-store 5 r0
-load r0 0
-store 6 r0
-afc r0 4
 store 7 r0
 load r1 7
-load r0 6
-inf r0 r0 r1
-store 6 r0
-load r1 6
-jmpc 84 r1
-load r0 0
-store 6 r0
-afc r0 1
+jmpc 44 r1
+afc r0 4
 store 7 r0
 load r0 7
-load r1 6
-add r0 r0 r1
-store 6 r0
-load r0 6
-store 0 r0
+store 5 r0
 load r0 1
-store 6 r0
+store 7 r0
+afc r0 1
+store 8 r0
+load r0 8
+load r1 7
+equ r0 r0 r1
+store 7 r0
+load r1 7
+jmpc 39 r1
 afc r0 2
 store 7 r0
 load r0 7
-load r1 6
-mul r0 r0 r1
 store 6 r0
-load r0 6
+jmp 43
+afc r0 4
+store 7 r0
+load r0 7
+store 6 r0
+jmp 67
+afc r0 5
+store 7 r0
+load r0 7
+store 5 r0
+load r0 1
+store 7 r0
+afc r0 1
+store 8 r0
+load r0 8
+load r1 7
+equ r0 r0 r1
+store 7 r0
+load r1 7
+jmpc 63 r1
+afc r0 6
+store 7 r0
+load r0 7
+store 6 r0
+jmp 67
+afc r0 3
+store 7 r0
+load r0 7
+store 6 r0
+load r0 0
+store 7 r0
+afc r0 4
+store 8 r0
+load r1 8
+load r0 7
+inf r0 r0 r1
+store 7 r0
+load r1 7
+jmpc 122 r1
+load r0 0
+store 7 r0
+afc r0 1
+store 8 r0
+load r0 8
+load r1 7
+add r0 r0 r1
+store 7 r0
+load r0 7
+store 0 r0
+load r0 1
+store 7 r0
+afc r0 2
+store 8 r0
+load r0 8
+load r1 7
+mul r0 r0 r1
+store 7 r0
+load r0 7
 store 1 r0
 load r0 0
-store 6 r0
+store 7 r0
 afc r0 3
+store 8 r0
+load r0 8
+load r1 7
+equ r0 r0 r1
+store 7 r0
+load r1 7
+jmpc 121 r1
+afc r0 6
 store 7 r0
 load r0 7
-load r1 6
-equ r0 r0 r1
-store 6 r0
-load r1 6
-jmpc 83 r1
-afc r0 6
-store 6 r0
-load r0 6
 store 4 r0
 load r0 4
-store 6 r0
+store 7 r0
 afc r0 3
+store 8 r0
+load r0 8
+load r1 7
+div r0 r0 r1
 store 7 r0
 load r0 7
-load r1 6
-div r0 r0 r1
-store 6 r0
-load r0 6
 store 3 r0
-jmp 29
+jmp 67
--- programme.c
+++ programme.c
@@ -5,12 +5,23 @@ void main(int arg){
 	int g;
 	int jam;
 	int harry;
+	int chris;
 	e=0;
 	d=1;
 	if (e!=0) {
 		harry=4;
+		if (d==1) {
+			chris=2;
+		} else {
+			chris=4;
+		}
 	} else {
 		harry = 5;
+		if (d==1) {
+			chris=6;
+		} else {
+			chris=3;
+		}
 	}
 	while (e < 4) {
 		e=e+1;
